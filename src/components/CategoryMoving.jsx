9133cdf9401bf9d7001160a33dd279da8098d2b5
sleepy2535 <greenapple5657@gmail.com>
2024-11-11 11:37:45 +0900

    Design : 움직이는 카테고리 css 수정

--- src/components/CategoryMoving.jsx
+++ src/components/CategoryMoving.jsx
@@ -4,26 +4,34 @@ import { createLoopingText } from "./CreateLoopingText";
 
 import coffee from "../assets/Coffee.png";
 import running from "../assets/PersonSimpleRun.png";
+import Racquet from "../assets/Racquet.png";
+import Baseball from "../assets/Baseball.png";
+import Wine from "../assets/Wine.png";
+import Mountain from "../assets/Mountains.png";
 
 function CategoryMoving() {
     const categories1 = [
-        { icon: running, text: "# 우리 동네 러닝크루", alt: "Running Icon" },
-        { icon: coffee, text: "# 분위기 좋은 카페 탐방", alt: "Coffee Icon" },
+        { icon: running, text: "# 우리 동네 러닝크루", alt: "running-icon" },
+        { icon: coffee, text: "# 분위기 좋은 카페 탐방", alt: "coffee-icon" },
     ];
     const categories2 = [
-        { icon: coffee, text: "# 도심 속 카페 투어", alt: "Coffee Icon" },
-        { icon: running, text: "# 아침 러닝 모임", alt: "Running Icon" },
+        { icon: Wine, text: "# 와인에 대해 알고 싶은", alt: "wine-icon" },
+        {
+            icon: Mountain,
+            text: "# 우리나라 산 정복하기",
+            alt: "mountain-icon",
+        },
     ];
     const categories3 = [
-        { icon: running, text: "# 주말 러닝클럽", alt: "Running Icon" },
-        { icon: coffee, text: "# 디저트 카페 탐방", alt: "Coffee Icon" },
+        { icon: Baseball, text: "# 요즘 유행 야구 직관", alt: "baseball-icon" },
+        { icon: Racquet, text: "# 배드민턴 같이 쳐요", alt: "racquet-icon" },
     ];
 
     useEffect(() => {
         const scrollElements = document.querySelectorAll(".scroll-content");
-        createLoopingText(scrollElements[0], 1); // line1: 왼쪽으로 이동
-        createLoopingText(scrollElements[1], -1); // line2: 오른쪽으로 이동
-        createLoopingText(scrollElements[2], 1); // line3: 왼쪽으로 이동
+        createLoopingText(scrollElements[0], 1);
+        createLoopingText(scrollElements[1], -1);
+        createLoopingText(scrollElements[2], 1);
     }, []);
 
     return (
@@ -35,10 +43,15 @@ function CategoryMoving() {
                         <div className="item">
                             {categories1.map((category, index) => (
                                 <div className="iconbox" key={index}>
-                                    <img
-                                        src={category.icon}
-                                        alt={category.alt}
-                                    />
+                                    <div
+                                        className={`iconimgbox ${category.alt}`}
+                                    >
+                                        <img
+                                            src={category.icon}
+                                            alt={category.alt}
+                                            className="imgbox"
+                                        />
+                                    </div>
                                     <div className="textbox">
                                         {category.text}
                                     </div>
@@ -51,10 +64,15 @@ function CategoryMoving() {
                                     className="iconbox"
                                     key={`clone1-${index}`}
                                 >
-                                    <img
-                                        src={category.icon}
-                                        alt={category.alt}
-                                    />
+                                    <div
+                                        className={`iconimgbox ${category.alt}`}
+                                    >
+                                        <img
+                                            src={category.icon}
+                                            alt={category.alt}
+                                            className="imgbox"
+                                        />
+                                    </div>
                                     <div className="textbox">
                                         {category.text}
                                     </div>
@@ -70,10 +88,15 @@ function CategoryMoving() {
                         <div className="item">
                             {categories2.map((category, index) => (
                                 <div className="iconbox" key={index}>
-                                    <img
-                                        src={category.icon}
-                                        alt={category.alt}
-                                    />
+                                    <div
+                                        className={`iconimgbox ${category.alt}`}
+                                    >
+                                        <img
+                                            src={category.icon}
+                                            alt={category.alt}
+                                            className="imgbox"
+                                        />
+                                    </div>
                                     <div className="textbox">
                                         {category.text}
                                     </div>
@@ -86,10 +109,15 @@ function CategoryMoving() {
                                     className="iconbox"
                                     key={`clone2-${index}`}
                                 >
-                                    <img
-                                        src={category.icon}
-                                        alt={category.alt}
-                                    />
+                                    <div
+                                        className={`iconimgbox ${category.alt}`}
+                                    >
+                                        <img
+                                            src={category.icon}
+                                            alt={category.alt}
+                                            className="imgbox"
+                                        />
+                                    </div>
                                     <div className="textbox">
                                         {category.text}
                                     </div>
@@ -105,10 +133,15 @@ function CategoryMoving() {
                         <div className="item">
                             {categories3.map((category, index) => (
                                 <div className="iconbox" key={index}>
-                                    <img
-                                        src={category.icon}
-                                        alt={category.alt}
-                                    />
+                                    <div
+                                        className={`iconimgbox ${category.alt}`}
+                                    >
+                                        <img
+                                            src={category.icon}
+                                            alt={category.alt}
+                                            className="imgbox"
+                                        />
+                                    </div>
                                     <div className="textbox">
                                         {category.text}
                                     </div>
@@ -121,10 +154,15 @@ function CategoryMoving() {
                                     className="iconbox"
                                     key={`clone3-${index}`}
                                 >
-                                    <img
-                                        src={category.icon}
-                                        alt={category.alt}
-                                    />
+                                    <div
+                                        className={`iconimgbox ${category.alt}`}
+                                    >
+                                        <img
+                                            src={category.icon}
+                                            alt={category.alt}
+                                            className="imgbox"
+                                        />
+                                    </div>
                                     <div className="textbox">
                                         {category.text}
                                     </div>
