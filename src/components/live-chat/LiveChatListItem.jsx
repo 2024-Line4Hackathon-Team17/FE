879a5c24b21e36e445af421d9b14cf133c67227a
BB545 <black1016@hanmail.net>
2024-11-14 16:04:58 +0900

    Feat: 채팅방 목록 조회 데이터 연결 중 사용자별 목록, 데이터 제대로 요청 되는지 테스트 필요

--- src/components/live-chat/LiveChatListItem.jsx
+++ src/components/live-chat/LiveChatListItem.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const LiveChatListItem = () => {
+const LiveChatListItem = ({ chatRoom }) => {
   return (
     <div className='live_chat_list_item_container'>
       <div className="live_chat_list_item_inner_container">
         <div className="live_chat_profile">
           <img src="https://i.namu.wiki/i/T3_Yf14kjCS9rcSAsQaD4EQIM1RF0x4VBJqP0tLlBzk4m3uTs3wGospUzcwB6LNuI4sz1o9XrrwPld13Hp7cMLuXBd1Kjvxca2xWkDh86XW9PH6n7cbxaOysPM6wR2tfwCQgikMD21x6pHTJLKAauA.webp" alt="프로필" />
         </div>
-        <Link to="/livechat/id">
+        <Link to={`/livechat/${chatRoom.chat_room_id}`}>
           <div className="live_chat_content btn">
             <div className="live_chat_nickname">
-              <p>미야옹</p>
+              <p>{chatRoom.other_user_name}</p>
             </div>
             <div className="live_chat_text">
-              <p>안녕하세요 성북천 러닝...</p>
+              <p>{chatRoom.last_message.length > 15
+                ? chatRoom.last_message.slice(0, 15) + '...'
+                : chatRoom.last_message}</p>
             </div>
           </div>
         </Link>
+        {chatRoom.unread_count > 0 ? (
+          <div className="unread_count">
+            <p>{chatRoom.unread_count}</p>
+          </div>
+        ) : (
+          <div className="live_chat_unread_box"></div>
+        )}
       </div>
     </div>
   )
--- src/pages/live-chat/LiveChatListPage.jsx
+++ src/pages/live-chat/LiveChatListPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LiveChatListItem from '../../components/live-chat/LiveChatListItem'
 import LiveChatSearch from '../../components/live-chat/LiveChatSearch'
 import EmpathyHeader from '../../components/empathy-community/EmpathyHeader'
+import axios from 'axios'
 
 const LiveChatListPage = () => {
+    const [chatRooms, setChatRooms] = useState([]);
+
+    useEffect(() => {
+        const fetchChatRooms = async () => {
+            try {
+                const response = await axios.get('/api/chatrooms/');
+                setChatRooms(response.data.chat_rooms);
+            } catch (error) {
+                console.error('Failed to fetch chat rooms:', error);
+            }
+        };
+
+        fetchChatRooms();
+    }, []);
+
     return (
         <div className='live_chat_list_container container'>
             <div className="live_chat_list_inner_container">
@@ -15,14 +31,12 @@ const LiveChatListPage = () => {
                 </div>
                 <LiveChatSearch />
                 <div className="live_chat_list_scroll">
-                    <LiveChatListItem />
-                    <LiveChatListItem />
-                    <LiveChatListItem />
-                    <LiveChatListItem />
-                    <LiveChatListItem />
-                    <LiveChatListItem />
-                    <LiveChatListItem />
-                    <LiveChatListItem />
+                    {chatRooms.map((chatRoom) => (
+                        <LiveChatListItem
+                            key={chatRoom.chat_room_id}
+                            chatRoom={chatRoom}
+                        />
+                    ))}
                 </div>
                 <div className='main_blank'></div>
             </div>
